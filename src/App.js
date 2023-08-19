130a1ea527fdce0a16198e7ea32d8d43576bf78d
madhusudan <madhusudanghulev@gmail.com>
2023-08-19 23:50:25 +0530

    web app initial commit~

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Index1 from './Component1/Index1';
+
+function App() {
+  return (
+    <>
+    
+    <div className="App">
+      <h1>web application task</h1>
+    </div>
+    <Index1/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component1/Index1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Index1() {
+  return (
+    <div>Index</div>
+  )
+}
+
+export default Index1
