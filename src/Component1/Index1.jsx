67e3c0d8011860fb2d306d38596fc5dd2fb852cc
madhusudan <madhusudanghulev@gmail.com>
2023-08-20 11:59:53 +0530

    data fetch for api and disply

--- src/Component1/Index1.jsx
+++ src/Component1/Index1.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Style from './index.module.css'
 function Index1() {
+  const [products, setProducts] = useState()
+
+  const fetchProduct = async () => {
+    const res = await fetch('https://dummyjson.com/products')
+    const data = await res.json()
+
+    console.log(data)
+    if (data && data.products) {
+      setProducts(data.products)
+    }
+  }
+  console.log(products)
+
+  useEffect(() => {
+    fetchProduct()
+
+  }, [])
+
   return (
-    <div>Index</div>
+    <>
+
+
+      {products && products.length > 0 && (
+        <div className={Style.products}>
+          {products.map((product) => {
+            return (
+              <div className={Style.productItem} key={product.id}>
+                <img src={product.thumbnail} alt={product.title} className={Style.productImage}/>
+                <br/>
+                <span className={Style.productTitle}>{product.title}</span>
+
+              </div>
+            )
+          })}
+        </div>
+      )}
+
+
+
+    </>
+
+
+
+
   )
 }
 
